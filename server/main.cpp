484e1b64c37494d2c4c9beb118b028307105afd3
vedant527 <vedant.bhat527@gmail.com>
2023-12-27 00:39:52 -0500

    init

--- server/main.cpp
+++ server/main.cpp
@@ -1,5 +1,12 @@
-#include <iostream>
 
-int main() {
-    std::cout << "Server" << std::endl;
+#include <iostream>
+#include <tcp_server.h>
+
+int main(int argc, char* argv[]) {
+
+    CSP::TCPServer server {CSP::IPV::V4, 1337};
+
+    server.Run();
+
+    return 0;
 }
